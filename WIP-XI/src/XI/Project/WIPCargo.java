70400da46d466b1b9eb07a26db6552320f6cc994
BedrockZ <50834840+BedrockZ@users.noreply.github.com>
2019-05-31 11:02:25 +0700

    commit project

--- WIP-XI/src/XI/Project/WIPCargo.java
+++ WIP-XI/src/XI/Project/WIPCargo.java
@@ -1,30 +1,61 @@
 /*
  * WIP XI Computer Programing 
  */
-
 package XI.Project;
+
+import java.util.Scanner;
 
 /**
  *
  * @author sittiwatlcp
  */
 public class WIPCargo {
+
     public static void main(String[] args) {
-        int age = 18;
-        String name = "Wippo";
-        double money = 1100.11;
-        
-        boolean online = true;
-        String status;
-        
-        System.out.println("Hi! my name is " + name + " i'm " + age + " Y/O ");
-        
-        if(online == true){
-            status = "Online";
-        } else {
-            status = "Offline";
+        int item = 5;
+        int limit = 10;
+        int menu;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("menu : ");
+        menu = sc.nextInt();
+        if (menu == 1) {
+            if(item < limit){
+                System.out.print("ใส่จำนวนที่จะ ADD : ");
+                int amount = sc.nextInt();
+                if(amount+item <= limit){
+                    item =+ amount;
+                    System.out.println("ADD เรียบร้อย");
+                } else {
+                    System.out.println("item ที่จะแอด มากกว่า limit");
+                }
+            } else {
+                System.out.println("เต็มจ้า");
+            }
         }
         
-        System.out.println("I'm currently " + status + " and have " + money + "THB. left.");
+        else if(menu == 2){
+            if(item > 0 ){
+                System.out.print("ใส่จำนวนที่จะ remove : ");
+                int amount = sc.nextInt();
+                if(amount-item < 0){
+                 
+                    System.out.println("remove เรียบร้อย");
+                } else {
+                    item -= amount ;
+                    System.out.println("item : " + item +",limit :" + limit);
+                }       
+            } else { 
+                    System.out.println("ไม่มีของในโกดังจ้า");
+                             
+            }
+        }
+        
+        else if(menu == 3){
+            System.out.print("item" + item);
+        } else {
+            System.out.println("ไปเลือกใหม่!!!!");
+            
+        }
+
     }
 }
